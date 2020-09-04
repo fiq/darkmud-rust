4a15e230c32c261bdf1a9e4fd22a93333f918e63
Raf Gemmail <rafiq@dreamthought.com>
2020-09-04 23:03:08 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Darkmud
+
+* Mess around with Rust which may or may not turn into a Mud
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+fn main() {
+    println!("Hello, world!");
+}
